284321428b8d4689cf0861b859faaeaecec82e8c
Óscar Rodrigues <oscarmsrodrigues@gmail.com>
2019-10-10 23:37:44 +0100

    info changes on main page

--- js/data.js
+++ js/data.js
@@ -5,56 +5,56 @@ new Vue({
             {
                 name: "0 + 1 = SOM",
                 description:
-                    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    "Created as a partnership with Braga Media Arts, it is a software and education programme with the goal of stimulating musical creation through logic and reasoning.",
                 image: "img/software/01som.png",
                 url: "https://01som.casadamusica.com/"
             },
             {
                 name: "Caça Sons",
                 description:
-                    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    "Created as a partnership with the Porto City Hall, it is a software and education programme with the goal of stimulating deep listening and focus through music.",
                 image: "img/software/cacasons.png",
                 url: "http://digitopia.casadamusica.com/sons"
             },
             {
                 name: "DigiDrone",
                 description:
-                    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    "Created as a partnership with Jazz ao Centro Clube, it is a software for music making, used as a tool for improvisation and music teaching.",
                 image: "img/software/digidrone.png",
                 url: "http://digitopia.casadamusica.com/DigiDrone"
             },
             {
                 name: "Caixa de Ritmos",
                 description:
-                    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    "An ongoing project for the Caixa de Ritmos workshop by Paulo Neto and António Miguel.",
                 image: "img/software/caixaritmos.png",
                 url: "http://digitopia.casadamusica.com/CaixaRitmos"
             },
             {
                 name: "DigiGranular",
                 description:
-                    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    "Currently under development, DigiGranular is a web-based granular synthesis instrument.",
                 image: "img/software/digigranular.png",
                 url: "http://digitopia.casadamusica.com/DigiGranular"
             },
             {
                 name: "DigiSonic",
                 description:
-                    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    "Currently under development, DigiSonic is a website focused on live coding in music, to be used by portuguese students.",
                 image: "img/software/digisonic.png",
                 url: "http://digitopia.casadamusica.com/DigiSonic"
             },
             {
                 name: "NepTune",
                 description:
-                    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    "Created as a partnership SPACE, a Erasmus+ project, NepTune is a tool to be used in STEAM education, using the planets from our solar system as musical instruments.",
                 image: "img/software/neptune.png",
                 url: "https://neptune.nunoh.com/"
             },
             {
                 name: "Gamult",
                 description:
-                    "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    "Gamult was created as a controller for Casa da Música's Robotic Gamelan, and it is available in both the App Store and Play Store.",
                 image: "img/software/gamult.png",
                 url: "https://digitopia.casadamusica.com/Gamult/"
             }
@@ -68,38 +68,38 @@ new Vue({
         team: [
             {
                 name: "Óscar Rodrigues",
-                position: "Curator",
-                about: "Lorem ipsum dolor sit amet.",
+                position: "Curator, workshop leading, performance",
+                about: "Óscar Rodrigues is a composer and educator from Porto. A member of the team since 2010, he's the current curator.",
                 image: "img/team/oscar.jpg"
             },
             {
                 name: "Pedro Augusto",
-                position: "Curator",
-                about: "Lorem ipsum dolor sit amet.",
+                position: "Sound art, performance",
+                about: "Pedro Augusto is an artist and musician working in the fields of electronic music, performance art and installation.",
                 image: "img/team/augusto.jpg"
             },
             {
                 name: "Nuno Hespanhol",
-                position: "Curator",
-                about: "Lorem ipsum dolor sit amet.",
+                position: "Web development and programming",
+                about: "Nuno is in charge of most of the code on Digitópia's projects. He is also a musician (studied classical guitar).",
                 image: "img/team/nuno.png"
             },
             {
                 name: "Filipe Fernandes",
-                position: "Curator",
-                about: "Lorem ipsum dolor sit amet.",
+                position: "Workshop leading, interactivity, performance",
+                about: "Filipe is a composer specializing in electronics and community projects.",
                 image: "img/team/filipe.jpg"
             },
             {
                 name: "Manuel Brásio",
-                position: "Curator",
-                about: "Lorem ipsum dolor sit amet.",
+                position: "Video, performance",
+                about: "Manuel is a composer and also handles Digitópia's video projects.",
                 image: "img/team/brasio.jpg"
             },
             {
                 name: "Ricardo Vieira",
-                position: "Curator",
-                about: "Lorem ipsum dolor sit amet.",
+                position: "Sound design, interactivity, performance",
+                about: "Ricardo is a Jazz pianist specializing in electronics, performance, and sound installations.",
                 image: "img/team/ricardo.jpg"
             },
         ]
@@ -111,29 +111,29 @@ new Vue({
     data: {
         noticias: [
             {
-                title: "Lançamento na App Store",
-                subtitle: "Light card title",
-                text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+                title: "Coming soon",
+                subtitle: "Late October 2019",
+                text: "This space is dedicated to the communication of Digitópia's activities, and its research."
             },
             {
-                title: "Lançamento na App Store",
-                subtitle: "Light card title",
-                text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+                title: "Coming soon",
+                subtitle: "Late October 2019",
+                text: "This space is dedicated to the communication of Digitópia's activities, and its research."
             },
             {
-                title: "Lançamento na App Store",
-                subtitle: "Light card title",
-                text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+                title: "Coming soon",
+                subtitle: "Late October 2019",
+                text: "This space is dedicated to the communication of Digitópia's activities, and its research."
             },
             {
-                title: "Lançamento na App Store",
-                subtitle: "Light card title",
-                text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+                title: "Coming soon",
+                subtitle: "Late October 2019",
+                text: "This space is dedicated to the communication of Digitópia's activities, and its research."
             },
             {
-                title: "Lançamento na App Store",
-                subtitle: "Light card title",
-                text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+                title: "Coming soon",
+                subtitle: "Late October 2019",
+                text: "This space is dedicated to the communication of Digitópia's activities, and its research."
             },
         ]
     }
